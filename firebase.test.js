beb2250cc1da5e6a79cb9552435e06f03d6c8ce8
mateusb12 <mateus_b09@hotmail.com>
2023-03-08 14:27:29 -0300

    unit test

--- firebase.test.js
+++ firebase.test.js
@@ -1,24 +1,15 @@
-// Import the Database class
 const Database = require('./index.js');
-
-// Create a new instance of the Database class
 const db = new Database();
 
+
+
 // Write a test case for the retrieveMessageFromDatabase function
-test('retrieveMessageFromDatabase should return a premise from the specified path', async () => {
-    // Mock the database.ref and database.ref.once functions
-    db.db.ref = jest.fn().mockReturnThis();
-    db.db.ref.once = jest.fn().mockResolvedValue('Hello World');
+test('retrieveMessageFromDatabase should return a premise with the correct data', async () => {
+    let expectedData = {"message": "Hello World!", "sender": "Alice"}
 
-    // Call the retrieveMessageFromDatabase function with a path
-    const premise = await db.retrieveMessageFromDatabase('dummy_messages');
-
-    // Expect that the database.ref function was called with the path
-    expect(db.db.ref).toHaveBeenCalledWith('dummy_messages');
-
-    // Expect that the database.ref.once function was called with 'value'
-    expect(db.db.ref.once).toHaveBeenCalledWith('value');
-
-    // Expect that the premise is 'Hello World'
-    expect(premise).toBe('Hello World');
+    const firebaseResult = await db.retrieveMessageFromDatabase('dummy_messages');
+    let output = Object.values(firebaseResult)[0]
+    expect(output).toEqual(expect.objectContaining(
+        expectedData
+    ));
 });
